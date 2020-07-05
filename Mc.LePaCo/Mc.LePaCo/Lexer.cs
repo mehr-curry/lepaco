a8288d9466e01f2e14bc7886237f45975a5db439
dario.beyer <dario.beyer@outlook.de>
2020-07-05 20:18:05 +0200

    first something

---
+++ Mc.LePaCo/Mc.LePaCo/Lexer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Mc.LePaCo
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <example>(Object.Property='123' or Object.Property.Property=1) and "1"&lt;&gt;"2"</example></example>
+    public class Lexer
+    {
+        public static IReadOnlyCollection<string> AvailableTokens = new ReadOnlyCollection<string>(
+            new List<string>
+            {
+                // logische Operatoren
+                "and", "or", "xor",
+                // Gruppierungsoperatoren
+                "(", ")",
+                // Vergleichsoperatoren
+                "=", "<>", "<", ">",
+                // Dereferenzierung
+                ".",
+                // Auswahl
+                "in", ",",
+                // Identifier
+                "[", "]",
+                // Stringliterale
+                "\"", "'"
+            }
+        );
+
+        public IEnumerable<string> Tokenize(Stream stream)
+        {
+            using (var reader = new StreamReader(stream))
+            {
+                while(reader.read)
+
+            }
+        }
+    }
+}
